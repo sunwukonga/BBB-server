e3340353039327eaf17bf0d122b66ca6ccb525a4
Paul Parker <pauldesmondparker@gmail.com>
2018-04-19 00:50:50 +0800

    Initial commit for createListing

--- data/resolvers.js
+++ data/resolvers.js
@@ -7,11 +7,13 @@ import {
   , View
   , OnlineStatus
   , Country
+  , SaleMode
   , Image
   , FortuneCookie
   , Facebook
   , Oauth
   , Email
+  , AWSS3
 } from './connectors';
 
 const FooError = createError('FooError', {
@@ -37,7 +39,7 @@ const resolvers = {
   Mutation: {
     loginFacebook(_, args) {
       var names;
-	  var auth;
+    var auth;
       return Facebook.login( args )
       .then( res => {
         console.log(res);
@@ -58,7 +60,7 @@ const resolvers = {
       .then( ([oauth, oauthCreated]) => {
         if (oauthCreated) {
           console.log("Oauth record was created.");
-		  auth = oauth;
+      auth = oauth;
           return Email.findOrCreate({
               where: { email: oauth.email }
             , defaults: {
@@ -68,17 +70,17 @@ const resolvers = {
           .then( ([email, emailCreated]) => {
             if (emailCreated) {
               // Email didn't exist, therefore no user existed. Create new.
-			  // In future, need to check user context.userid
+        // In future, need to check user context.userid
               console.log("Email didn't exist AND was created");
-			  var nickname = names.join('');
-			  var lastName = names.pop();
-			  var firstName = names.join(' ');
+        var nickname = names.join('');
+        var lastName = names.pop();
+        var firstName = names.join(' ');
               return User.findOrCreate({
                   where: { firstName: firstName }
                 , defaults: {
                     lastName: lastName
-				  , profileName: nickname,
-				}
+                  , profileName: nickname,
+                }
               })
               .then( ([user, userCreated]) => {
                 if (! userCreated) {
@@ -102,7 +104,7 @@ const resolvers = {
           }).then( user => {
             console.log("Attempting to access user to create jwt token");
             console.log(user);
-            var userToken = {
+            let userToken = {
                 "userid": user.id
               , "role": [
                   {
@@ -112,9 +114,67 @@ const resolvers = {
             }
             return jwt.sign( JSON.stringify(userToken), process.env.JWT_SECRET_KEY );
           })
+        } else {
+          let userToken = {
+              "userid": oauth.userId
+            , "role": [
+                {
+                   "name": "GENERAL"
+                }
+              ]
+          }
+          return jwt.sign( JSON.stringify(userToken), process.env.JWT_SECRET_KEY );
         }
       })
     },
+    getSignedUrl(_, args, context) {
+      if (context.userid !== "") {
+        return AWSS3.getSignedUrl( args )
+      }
+    },
+    createListing(_, args, context) {
+      SaleMode.create({
+          mode: args.mode
+        , price: args.cost
+        , counterOffer: args.counterOffer
+        , currencyId: args.currency
+      })
+      .then( mode => {
+        switch( args.mode ) {
+          case 'SALE':
+          case 'DONATE':
+          case 'BARTER':
+          case 'SALEBARTER':
+
+        }
+        return Listing.create({
+            title: args.title
+          , description: args.description
+          , userId: context.userid
+          , categoryId: args.category
+          , templateId: args.template
+          , saleModeId: mode.id
+        })
+      })
+      .then( listing => {
+        listing.setTags( args.tags.map( tagId => Tags.findOne({
+            where: {id: tagId},
+          }));
+        listing.addMode();
+      })
+      //   createListing: context.userid, title, description, category
+      //   addTemplate: template, tags
+      //   createMode: mode{
+      //     SALE: currency, cost, counterOffer
+      //     BARTER: barterTemplates
+      //     SALEBARTER: currency, cost, counterOffer, barterTemplates
+      //     DONATE:
+      //     }
+      //   createDelivery: method{ftf, post}, address, cost, currency
+      //   map over images, if discarded: delete S3 record and image ref in database, 
+      //                            else: update Image with URL
+      //   
+    }
   },
   User: {
     listings(user) {
