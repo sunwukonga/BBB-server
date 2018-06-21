d42c030344fb2cecd64009485df51668a54c694f
Paul Desmond Parker <pauldesmondparker@gmail.com>
2018-06-21 20:42:58 +0800

    Fix image upload

--- data/connectors.js
+++ data/connectors.js
@@ -40,32 +40,36 @@ const s3 = new AWS.S3();
 const AWSS3 = {
   async getSignedUrl( args ) {
     return await ImageModel.create({})
-      .then( image => createOpaqueUniqueImageKey(image.id) )
-      .then( uniqKey => s3.createPresignedPost({
-        Bucket: BBB_BUCKET
-        , Conditions: [
-           ["content-length-range", 0, 262144],
-           [ "eq", "$acl", "public-read" ]
-        ]
-        , Fields: {
-          key: uniqKey
-        }
-        , ContentType: args.imageType
-        }))
-      .then( data => {
-        image.imageKey = data.fields.key;
-        image.save();
-        console.log("data: ", data)
-        return {
-            id: image.id
-          , key: data.fields.key
-          , bucket: data.fields.bucket
-          , X_Amz_Algorithm: data.fields['X-Amz-Algorithm']
-          , X_Amz_Credential: data.fields['X-Amz-Credential']
-          , X_Amz_Date: data.fields['X-Amz-Date']
-          , policy: data.fields.Policy
-          , X_Amz_Signature: data.fields['X-Amz-Signature']
-        }
+      .then( image => {
+        return createOpaqueUniqueImageKey(image.id)
+        .then( uniqKey => {
+          return s3.createPresignedPost({
+            Bucket: BBB_BUCKET
+            , Conditions: [
+               ["content-length-range", 0, 262144],
+               [ "eq", "$acl", "public-read" ]
+            ]
+            , Fields: {
+              key: uniqKey
+            }
+            , ContentType: args.imageType
+            })
+          .then( data => {
+            image.imageKey = data.fields.key;
+            image.save();
+            console.log("data: ", data)
+            return {
+                id: image.id
+              , key: data.fields.key
+              , bucket: data.fields.bucket
+              , X_Amz_Algorithm: data.fields['X-Amz-Algorithm']
+              , X_Amz_Credential: data.fields['X-Amz-Credential']
+              , X_Amz_Date: data.fields['X-Amz-Date']
+              , policy: data.fields.Policy
+              , X_Amz_Signature: data.fields['X-Amz-Signature']
+            }
+          })
+        })
       })
   },
   async deleteObject( key ) {
